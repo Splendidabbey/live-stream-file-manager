0e221e519fee0426e6eb763ac78fbdbc3e398229
Abiodun Olorunda <splendidabbey@gmail.com>
2023-08-24 06:52:21 +0100

    new commit

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,18 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$GdfGpauv$zsY3ZRECOdRsI2R2qXu/j1',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'user1',
+    'pass' => '$1$5086Kcnx$G9twYz80d0Gcv0tJUaG.w0',
+    'role' => 'admin',
+    'dir' => '["User1"]',
+    'quota' => '1024',
+  ),
+);
